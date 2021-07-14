f2e99424889e249dee90994d578e8355698b2d53
Ethan-EunjiLee <ethan.ej.lee@gmail.com>
2021-07-14 09:39:05 +0900

    7/14 14.2 TailwindCss part Two

---
+++ src/postcss.config.js
@@ -0,0 +1,9 @@
+// * https://tailwindcss.com/docs/installation 참조
+// * tailwind를 일반 css파일로 빌드하기 위해 필요
+// postcss.config.js
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// * tailwind를 확장해서 사용하기 위해 설정 필요
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
