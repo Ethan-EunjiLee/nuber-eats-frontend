ce582f3d76d2159ed226d824d98cffff458cbcbc
Ethan-EunjiLee <ethan.ej.lee@gmail.com>
2021-07-27 09:12:33 +0900

    7/27 15.8 Login Mutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
-import { gql, useMutation } from "@apollo/client";
+import { ApolloError, gql, useMutation } from "@apollo/client";
 import {
   loginMutation,
   loginMutationVariables,
@@ -16,10 +16,13 @@ interface ILoginForm {
 /**
  * * mutation 이름을 설정해줄 수 있지만(Potato Mutation), 이 이름이 back-end로 넘어가는건 아니다. Front-End를 위한 영역
  * * $: Apollo에게 이게 변수라는 것을 알려주는 역할(변수마다 처음 1번만 적어주면 된다)
+ * * 예전거
+ * * mutation loginMutation($email: String!, $password: String!) {
+ * *    login(input: { email: $email, password: $password }) {
  */
 const LOGIN_MUTATION = gql`
-  mutation loginMutation($email: String!, $password: String!) {
-    login(input: { email: $email, password: $password }) {
+  mutation loginMutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
       ok
       token
       error
@@ -34,12 +37,43 @@ export const Login = () => {
     getValues,
     formState: { errors }, //* errors를 이용하면 validation 후 message 출력 기능 구현 가능  */
     handleSubmit,
+    watch, // * 실시간으로 변화를 감지
   } = useForm<ILoginForm>();
 
-  const [loginMutation, { data }] = useMutation<
+  // * useMutation의 옵션으로 넣어줄 onCompleted 함수 구현
+  const onCompleted = (loginMutationResult: loginMutation) => {
+    const {
+      login: { error, ok, token },
+    } = loginMutationResult;
+    if (ok) {
+      console.log("token: ", token);
+    }
+  };
+
+  // * useMutation의 옵션으로 넣어줄 onError 함수 구현
+  // * ApolloError: 연결이 안되면 발생 => backend에서 request를 받아주지 않아서, 인증이 필요하거나, url이 잘못됐을 경우 등등
+  // * backend에서 OutputDto에 만들어준 error가 아님
+  const onError = (error: ApolloError) => {};
+
+  // * useMutation의 파라미터로 mutation에 필요한 파라미터를 바로 넣어줄 수 있다.
+  // * data: loginMutationResult => data 변수명을 loginMutationResult로 변경
+  const [loginMutation, { data: loginMutationResult }] = useMutation<
     loginMutation,
     loginMutationVariables
-  >(LOGIN_MUTATION);
+  >(LOGIN_MUTATION, {
+    // * BaseMutationOptions<TData,TVariables>.onCompleted?: ((data: loginMutation) => void) | undefined
+    onCompleted,
+    // * BaseMutationOptions<TData,TVariables>.onError?: ((error: ApolloError) => void) | undefined
+    onError,
+    // * 아래 항목으로 하면 실시간으로 바로바로 값이 변화한다.
+    // * 여기에 넣거나 찐 Mutation 안에 넣거나 하면 된다.
+    // variables: {
+    //   loginInput: {
+    // email: watch("email"), // * watch가 데이터 변화를 감지한다.
+    // password: watch("password"),
+    //   },
+    // },
+  });
   // * 이 배열의 첫 변수는 mutation function
   // * loading: mutation이 실행되고 있다는 의미
   // * data: 실제로 backend에서 넘어온 데이터
@@ -49,9 +83,12 @@ export const Login = () => {
     const { email, password } = getValues();
     // * codegen을 이용해 만들어준 interface를 useMutation의 type으로 설정해줬기 때문에 variables 타입들이 알아서 지정된다.
     loginMutation({
+      // * 아래 항목은 원래 loginMutaion의 파라미터로 넣어준 값
       variables: {
-        email,
-        password,
+        loginInput: {
+          email,
+          password,
+        },
       },
     });
   };
@@ -107,6 +144,10 @@ export const Login = () => {
             // </span>
           )}
           <button className="btn mt-3">Login</button>
+          {/* backend 연결 후 넘어온 output에서 error값이 존재할 경우*/}
+          {loginMutationResult?.login.error && (
+            <FormError errorMessage={loginMutationResult.login.error} />
+          )}
         </form>
       </div>
     </span>
